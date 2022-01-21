53c99d4521fc67184d6ede085c959910e96d408f
kavindya <kavindyajapura@gmail.com>
2022-01-21 14:45:01 -0800

    login page

--- project_1/src/app/app.module.ts
+++ project_1/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
